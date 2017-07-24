0deb90ec0aafc08388e7012af7c38f4dd3b699f3
Carlos Salazar <Carlos@Carloss-MBP-3.hsd1.tx.comcast.net>
2017-07-23 22:42:27 -0500

    all day events and regular events have different colors now

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,37 +4,40 @@
 
 import Ember from 'ember';
 
-const { Logger: { log }, $ } = Ember;
-
 export default Ember.Controller.extend({
   id: idGenerator(),
   dayAspectRatio: 1.1,
   selectedDay: new moment(),
-  events: Ember.A([{
-    id: idGenerator(),
-    title: 'Event 1',
-    className: 'regular-event',
-    start: '2017-07-20T07:30:00',
-    end: '2017-07-20T09:00:00',
-    editable: true,
-    content: ''
-  },
-  {
-    id: idGenerator(),
-    title: 'Generated Event',
-    className: 'regular-event',
-    start: new moment().minute(0).second(0),
-    editable: true,
-    content: ''
-  },  {
-    id: idGenerator(),
-    title: 'All Day Event',
-    className: 'all-day-event',
-    allDay: true,
-    start: '2017-07-19',
-    editable: true,
-    content: ''
-  }
+  events: Ember.A([
+    {
+      id: idGenerator(),
+      title: 'Event 1',
+      className: 'regular-event',
+      start: '2017-07-20T07:30:00',
+      end: '2017-07-20T09:00:00',
+      editable: true,
+      content: '',
+      color: '#337ab7'
+    },
+    {
+      id: idGenerator(),
+      title: 'Generated Event',
+      className: 'regular-event',
+      start: new moment().minute(0).second(0),
+      editable: true,
+      content: '',
+      color: '#337ab7'
+    },  
+    {
+      id: idGenerator(),
+      title: 'All Day Event',
+      className: 'all-day-event',
+      allDay: true,
+      start: '2017-07-19',
+      editable: true,
+      content: '',
+      color: '#f00'
+    }
   ]),
 
   actions: {
@@ -43,27 +46,27 @@ export default Ember.Controller.extend({
       // here we have to set the controller events object because map returns a copy of the array and does not mutate it
       var updatedEvents = this.get('events').map(e => {
         if (e.id === event.id) {
-          return event
+          if (event.allDay){
+            event.color = '#f00';
+            return event;
+          } else {
+            event.color = '#337ab7';
+            return event;
+          }
         }
         return e;
       });
       this.set('events', updatedEvents);
     },
-
     // month calendar interactions
     dayClicked: function(date, jsEvent, view) {
       // takes the user to the correct day on the agenda unless theya re editing an event
-      if (!this.get('editingEventDetails')) {
-        this.set('selectedDay', date);
-      }
+      if (!this.get('editingEventDetails')) { this.set('selectedDay', date); }
     },
     eventClicked: function(event, jsEvent, view) {
       // takes the user to the correct day on the agenda unless they are editing an event
-      if (!this.get('editingEventDetails')) {
-        this.set('selectedDay', event.start);
-      }
-    },
-    
+      if (!this.get('editingEventDetails')) { this.set('selectedDay', event.start); }
+    },   
     // day agenda interactions
     editEvent: function(event,element,view) { 
       this.set('editingEventDetails',true);
@@ -74,6 +77,7 @@ export default Ember.Controller.extend({
       var events = this.get('events');
       // all day events only contain information about the day, thus format will return 00:00:00
       var allDay = date.format('HH:mm:ss') === "00:00:00" ? true : false;
+      var color = allDay ? '#f00' : '#337ab7';
       // creates object with some default parameters
       // use Click to edit to idicate the appropriate user interaction to take
       var newEvent = Ember.Object.create({
@@ -83,22 +87,19 @@ export default Ember.Controller.extend({
         start: date,
         editable: true,
         allDay,
-        content: ''
+        content: '',
+        color
       });
       // adds to the current set of user objects. this is an internal ember function
       // and will notify that a re render to should take place
       events.pushObject(newEvent);
     },
-    
     // event edit form actions
     saveEvent: function([event, title, content]) {
       event.title = title;
       event.content = content; 
-
       var updatedEvents = this.get('events').map(e => {
-        if (e.id === event.id) {
-          return event
-        }
+        if (e.id === event.id) { return event; }
         return e;
       });
       this.set('events', updatedEvents);
@@ -110,9 +111,7 @@ export default Ember.Controller.extend({
       this.set('event', null);
     },
     deleteEvent: function(event) {
-      var events = this.get('events').filter(e => {
-        return e.id !== event.id;
-      });
+      var events = this.get('events').filter(e => { return e.id !== event.id; });
       this.set('events', events);
       this.set('editingEventDetails', false);
       this.set('event', null); 
